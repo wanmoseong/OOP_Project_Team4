412d00d64caeb64813b55517ddf16c1f425fc447
Wanmo <97231408+wanmoseong@users.noreply.github.com>
2023-05-19 16:56:20 +0900

    Update StartPage.java

--- Bulid_Puzzle/src/StartPage.java
+++ Bulid_Puzzle/src/StartPage.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class StartPage extends JFrame {
+	
 	public StartPage() {
 		setTitle("Team4 Puzzle");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -24,20 +25,42 @@ public class StartPage extends JFrame {
 		Right.setBackground(Color.GRAY);
 		Right.setPreferredSize(new Dimension(400,400));
 		
+		//Left Part.
 		Left.setLayout(null);
 		
-		JButton btn = new JButton("Test");
-		btn.setBounds(100, 100, 100, 50);
+		JLabel Text = new JLabel("난이도 설정");
+		Text.setBounds(100, 50, 200, 50);
+		Left.add(Text);
 		
-		btn.addActionListener(new ActionListener() {
+		JButton btn3x3 = new JButton("3x3");
+		btn3x3.setBounds(100, 120, 200, 50);
+
+		btn3x3.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 				new mainPuzzle();
 			}
 		});
-		Left.add(btn);
+		Left.add(btn3x3);
 		
+		JButton btn4x4 = new JButton("4x4");
+		btn4x4.setBounds(100, 195, 200, 50);
+		
+		btn4x4.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				new Puzzle();
+			}
+		});
+		Left.add(btn4x4);
+		
+		JButton btn5x5 = new JButton("5x5");
+		btn5x5.setBounds(100, 270, 200, 50);
+		Left.add(btn5x5);
+		
+		//Right Part. 추후 추가 예정
 		
 		setVisible(true);
 		c.add(Body);
