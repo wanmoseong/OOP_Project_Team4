1afbf307df256eeb177c62395adadff13b0071ba
wogus35431 <133317305+wogus35431@users.noreply.github.com>
2023-05-26 15:45:39 +0900

    Update StartPage.java

--- Bulid_Puzzle/src/StartPage.java
+++ Bulid_Puzzle/src/StartPage.java
@@ -11,10 +11,11 @@ public class StartPage extends JFrame {
 	JScrollPane scrollPane;
     ImageIcon iconbody, iconleft, iconright;
     String ImagePath;
+    image im;
 
     
 	public StartPage() {
-		
+		 
 		 iconbody = new ImageIcon("Body.png"); //이미지 끌고오기
 		 
 		 JPanel Body = new JPanel() { 
@@ -42,10 +43,6 @@ public class StartPage extends JFrame {
 				 g.drawImage(iconright.getImage(), 0, 0, getWidth(), getHeight(), null);;
 	         }
 		 };//사진만 넣어놨습니다 이미지 버튼 X
-		 
-		 
-	    image im = new image(Right);		// 오른쪽 패널에 파일 선택 "열기" 버튼 추가
-	    ImagePath = im.AbsolutePath;		// 이미지의 절대경로를 ImagePath에 저장함.
 	
 		 
 		iconleft = new ImageIcon("startpageex.png"); //이미지 끌고오기
@@ -88,50 +85,52 @@ public class StartPage extends JFrame {
 		JButton btn3x3 = new JButton("3x3");
 		btn3x3.setBackground(Color.WHITE);
 		btn3x3.setBounds(100, 170, 200, 50);
-		
-		
-		
-		btn3x3.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				new mainPuzzle(3);
-			}
-		});
 		Left.add(btn3x3);
 		
 		
 		JButton btn4x4 = new JButton("4x4");
 		btn4x4.setBackground(Color.WHITE);
 		btn4x4.setBounds(100, 240, 200, 50);
-		
-		btn4x4.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				new mainPuzzle(4);
-			}
-		});
 		Left.add(btn4x4);
 		
 		JButton btn5x5 = new JButton("5x5");
 		btn5x5.setBackground(Color.WHITE);
 		btn5x5.setBounds(100, 310, 200, 50);
-		
-		btn5x5.addActionListener(new ActionListener() {
-
-			public void actionPerformed(ActionEvent e) {
-				dispose();
-				new mainPuzzle(5);
-			}
-		});
 		Left.add(btn5x5);
 
 		
 		setVisible(true);
 		c.add(Body);
 		c.setBackground(Color.red);
-
+		
+		im = new image(Right);			// 오른쪽 패널에 파일 선택 "열기" 버튼 추가 파일 열기 전에는 액션 추가 ㄴㄴ
+		
+		System.out.println("File path selected: " + im.AbsolutePath);		// 경로가 잘 들어갔는지 확인
+		System.out.println("File path selected in main: " + im.AbsolutePath);
+                
+		btn3x3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			dispose();
+			new mainPuzzle(3,im.AbsolutePath);
+			}
+		});
+                
+		btn4x4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				new mainPuzzle(4,im.AbsolutePath);
+			}
+		});
+		
+		btn5x5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				new mainPuzzle(5,im.AbsolutePath);
+			}
+		});
+                
+		System.out.println("first time is sucess");		// 그냥 처음 시작했을 때, 성공은 하는데 뒤로가기 했을 때, 뭔가 꼬임;;
+	   
 	}
 
 	public static void main(String[] args) {
