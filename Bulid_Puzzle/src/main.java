9f46d3ec40491076bdfd48632355aefdb17b13fa
eunhyoha2 <127116866+eunhyoha2@users.noreply.github.com>
2023-05-26 16:53:54 +0900

    Add files via upload

---
+++ Bulid_Puzzle/src/main.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class main extends JFrame {
+	public static void main(String[] args) {
+		StartPage i = new StartPage();
+		System.out.println(i.ImagePath);
+	}
+
+}
