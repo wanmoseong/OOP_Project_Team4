cff1517c14877e028afa324c233bc5b8a8e902d4
Wanmo <97231408+wanmoseong@users.noreply.github.com>
2023-05-27 02:13:37 +0900

    Update mainPuzzle.java

--- Bulid_Puzzle/src/mainPuzzle.java
+++ Bulid_Puzzle/src/mainPuzzle.java
@@ -13,7 +13,7 @@ public class mainPuzzle extends JFrame {
     	
     	icon_p_body = new ImageIcon("Body.png"); //이미지 끌고오기
 		 
-		 JPanel p_body = new JPanel() { 
+		JPanel p_body = new JPanel() { 
 			 
 			 protected void paintComponent(Graphics g) {
 				 super.paintComponent(g);
@@ -28,16 +28,12 @@ public class mainPuzzle extends JFrame {
         setLocationRelativeTo(null); //실행하면 중앙에 위치 함 
         Container c = getContentPane();
 
-        
-        
         // 가운데
         p_right = new JPanel(); // p_right = 힌트 사진
         JPanel p_left = new JPanel(); // p_left = 실행화면
         
         p_right.setPreferredSize(new Dimension(400, 400));
         p_right.setBackground(Color.WHITE);
-        //p_left.setPreferredSize(new Dimension(400, 400));
-        //p_left.setBackground(Color.WHITE);
         
         String imagePath = ImagePath; // 실제 이미지 파일
         addImageToPanel(imagePath);
@@ -51,25 +47,25 @@ public class mainPuzzle extends JFrame {
 
         p_body.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
 	
-		
+		/*
 		JButton back = new JButton("뒤로가기");
 		back.setBounds(230, 450, 80, 30);
 
 		back.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-				dispose();
-				new StartPage();
+				
 			}
 		});
 		p_body.add(back);
-		
+		*/
 		JButton resetButton = new JButton("초기화");
 		resetButton.setBounds(100, 450, 80, 30);
 
 		resetButton.addActionListener(new ActionListener() {
 		    public void actionPerformed(ActionEvent e) {
-		        // 초기화 작업을 수행하는 코드를 여기에 작성하세요.
+		    	dispose();
+				new mainPuzzle(A, ImagePath);
 		    }
 		});
 
@@ -79,14 +75,25 @@ public class mainPuzzle extends JFrame {
 		exitButton.setBounds(350, 450, 80, 30);
 		exitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				dispose();
 				System.exit(0);
 			}
 		});
 		p_body.add(exitButton);
+		
+		TimerPanel timerPanel = new TimerPanel();
+		JButton resetButton1 = new JButton("시간 초기화");
+		resetButton1.setBounds(600, 450, 80, 30);
+		resetButton1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	  timerPanel.resetTimer();
+            }
+        });
+		p_body.add(resetButton1);
+		
     }
     
-     //우측 Panel 힌트 추가를 위한 메소드
+    
+     //우측 Panel 사진 추가를 위한 메소드
     private void addImageToPanel(String imagePath) {
         ImageIcon originalImageIcon = new ImageIcon(imagePath);
         Image originalImage = originalImageIcon.getImage();
@@ -101,11 +108,5 @@ public class mainPuzzle extends JFrame {
         p_right.revalidate();
     }
     
-    //좌측 Panel 퍼즐을 위한 클래스 생성과 메소드 추가를 위한 부분
-    /*************************************************/
-    /*                                               */
-    /*      이 부분에서 시간 카운트와 퍼즐구현이 이뤄짐 (아마도..)*/
-    /*                                               */
-    /*************************************************/
     
 }
