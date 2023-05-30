420867ab03a22c72d52ec7878f50619d2a47a7eb
solha <129755445+05solar@users.noreply.github.com>
2023-05-30 12:46:12 +0900

    Update mainPuzzle.java

--- Bulid_Puzzle/src/mainPuzzle.java
+++ Bulid_Puzzle/src/mainPuzzle.java
@@ -6,14 +6,15 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class mainPuzzle extends JFrame {
+	private TimerPanel timerPanel; // 타이머
     public JPanel p_right;
     ImageIcon icon_p_body, iconleft, iconright;
 
     public mainPuzzle(int A,String ImagePath) {
     	
-    	icon_p_body = new ImageIcon("Body.png"); //이미지 끌고오기
+    	icon_p_body = new ImageIcon("C:\\Users\\dlthf\\OneDrive\\바탕 화면\\객체지향팀플\\자바 예시 그림\\BodyImage.png"); //이미지 끌고오기
 		 
-		JPanel p_body = new JPanel() { 
+		 JPanel p_body = new JPanel() { 
 			 
 			 protected void paintComponent(Graphics g) {
 				 super.paintComponent(g);
@@ -28,72 +29,79 @@ public class mainPuzzle extends JFrame {
         setLocationRelativeTo(null); //실행하면 중앙에 위치 함 
         Container c = getContentPane();
 
+        timerPanel = new TimerPanel();
+        
+
+
+        
+        
         // 가운데
         p_right = new JPanel(); // p_right = 힌트 사진
         JPanel p_left = new JPanel(); // p_left = 실행화면
         
         p_right.setPreferredSize(new Dimension(400, 400));
         p_right.setBackground(Color.WHITE);
+        //p_left.setPreferredSize(new Dimension(400, 400));
+        //p_left.setBackground(Color.WHITE);
         
         String imagePath = ImagePath; // 실제 이미지 파일
         addImageToPanel(imagePath);
 
         p_body.add(new PicPanel(450,A,imagePath), BorderLayout.WEST);
         p_body.add(p_right, BorderLayout.EAST);
+       
         
+        p_body.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+        
+        
+        setVisible(true);
         c.add(p_body, BorderLayout.CENTER);
         
         setVisible(true);
 
         p_body.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
 	
-		/*
-		JButton back = new JButton("뒤로가기");
-		back.setBounds(230, 450, 80, 30);
+		
+		//JButton back = new JButton("뒤로가기");
+		//back.setBounds(230, 450, 80, 30);
 
-		back.addActionListener(new ActionListener() {
+		//back.addActionListener(new ActionListener() {
 
-			public void actionPerformed(ActionEvent e) {
-				
-			}
-		});
-		p_body.add(back);
-		*/
-		JButton resetButton = new JButton("초기화");
-		resetButton.setBounds(100, 450, 80, 30);
-
-		resetButton.addActionListener(new ActionListener() {
-		    public void actionPerformed(ActionEvent e) {
-		    	dispose();
-				new mainPuzzle(A, ImagePath);
-		    }
-		});
-
-		p_body.add(resetButton);
+			//public void actionPerformed(ActionEvent e) {
+				//dispose();
+				///new StartPage();
+			//}
+		//});
+		//p_body.add(back);
+		
+		
 		
 		JButton exitButton = new JButton("종료");
 		exitButton.setBounds(350, 450, 80, 30);
 		exitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				dispose();
 				System.exit(0);
 			}
 		});
 		p_body.add(exitButton);
-		
-		TimerPanel timerPanel = new TimerPanel();
-		JButton resetButton1 = new JButton("시간 초기화");
-		resetButton1.setBounds(600, 450, 80, 30);
-		resetButton1.addActionListener(new ActionListener() {
+
+		JButton resetButton = new JButton("시간 초기화");
+		resetButton.setBounds(100, 450, 80, 30);
+
+		resetButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             	  timerPanel.resetTimer();
             }
         });
-		p_body.add(resetButton1);
-		
+
+		p_body.add(resetButton);
+		p_body.add(timerPanel);
     }
     
     
-     //우측 Panel 사진 추가를 위한 메소드
+     //우측 Panel 힌트 추가를 위한 메소드
     private void addImageToPanel(String imagePath) {
         ImageIcon originalImageIcon = new ImageIcon(imagePath);
         Image originalImage = originalImageIcon.getImage();
@@ -109,4 +117,5 @@ public class mainPuzzle extends JFrame {
     }
     
     
+
 }
