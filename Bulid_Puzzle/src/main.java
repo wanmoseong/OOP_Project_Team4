040a0d944d4f36529f40ff23158f0d833deb235b
wogus35431 <133317305+wogus35431@users.noreply.github.com>
2023-06-01 13:29:27 +0900

    Update main.java

--- Bulid_Puzzle/src/main.java
+++ Bulid_Puzzle/src/main.java
@@ -1,9 +1,10 @@
+package TeamProject;
+
+
 import javax.swing.JFrame;
 
 public class main extends JFrame {
 	public static void main(String[] args) {
-		StartPage i = new StartPage();
-		System.out.println(i.ImagePath);
+		StartWindow i = new StartWindow();
 	}
-
 }
