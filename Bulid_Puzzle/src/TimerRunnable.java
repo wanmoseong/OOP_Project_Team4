fda69f5d4516a5eb0bdebc2c9c91c0b25f80acd1
Wanmo <97231408+wanmoseong@users.noreply.github.com>
2023-06-07 04:05:31 +0900

    Update TimerRunnable.java

--- Bulid_Puzzle/src/TimerRunnable.java
+++ Bulid_Puzzle/src/TimerRunnable.java
@@ -3,28 +3,33 @@ import java.awt.event.*;
 import javax.swing.*;
 
 class TimerRunnable implements Runnable {
-	 public JLabel timerLabel2, timerLabel3;
-     public Timer timer;
-     public int seconds;
-	 public JLabel timerLabel; 
-	 public TimerRunnable(JLabel timerLabel) {
+	public JLabel timerLabel2, timerLabel3;
+	public Timer timer;
+	public JLabel timerLabel;
+	public int seconds;
+	public int n = 0;
+	
+	public TimerRunnable() {}
+
+	public TimerRunnable(JLabel timerLabel) {
 		this.timerLabel = timerLabel;
-		}
-	 public void run() {
-		int n=0; 
-		while(true) { 
-			timerLabel.setText(Integer.toString(n)); 
-			n++; 
-			try {
-				Thread.sleep(1000); // 1초 동안 잠을 잔다.
-				}
-			catch(InterruptedException e) {
-				return; // 예외가 발생하면 스레드 종료
-				}
-			
-			}
-		}
-	 
 	}
 
-    
+	public void run() {
+		while (true) {
+			timerLabel.setText(Integer.toString(n));
+			n++;
+			try {
+				Thread.sleep(1000); // 1초 동안 잠을 잔다.
+			} catch (InterruptedException e) {
+				seconds = n;
+				return; // 예외가 발생하면 스레드 종료
+			}
+
+		}
+	}
+	public int getSeconds() {
+		return seconds;
+	}
+
+}
